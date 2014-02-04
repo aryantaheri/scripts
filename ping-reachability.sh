d753958aff5acf6f74795a0f73145ae98792078c
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-02-04 17:12:51 +0100

    Adding ping reachability script

---
+++ ping-reachability.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+
+#Time in milliseconds
+#$(($(date +%s%N)/1000000))
+start_time=$(($(date +%s%N)/1000000))
+
+((count = 100))
+
+if [ ! -z "$1" ] ;  then
+    dst=$1
+    echo 'Destination is: ' $dst
+fi
+
+if [ ! -z "$2" ] ;  then
+    ((count = $2))
+    echo 'Number of retries:' $count
+fi
+
+while [[ $count -ne 0 ]] ; do
+    ping -c 1 $dst                      # Try once.
+    rc=$?
+    if [[ $rc -eq 0 ]] ; then
+        ((count = 1))                      # If okay, flag to exit loop.
+    fi
+    ((count = count - 1))                  # So we don't go forever.
+done
+
+if [[ $rc -eq 0 ]] ; then                  # Make final determination.
+    echo 'say The internet is back up.'
+else
+    echo 'say Timeout.'
+fi
+
+end_time=$(($(date +%s%N)/1000000))
+echo 'Time to Reachability' `expr $end_time - $start_time` 'milliseconds'
+
