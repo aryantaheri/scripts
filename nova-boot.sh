b20262d8b296b53fa4327b84598df7cdc27d047a
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-02-05 17:07:07 +0100

    nova boot script for reachability time test

---
+++ nova-boot.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+((retries = 100))
+
+
+
+if [ ! -z "$1" ] ;  then
+    image=$1
+    echo 'Image UUID is: ' $image
+fi
+
+if [ ! -z "$2" ] ;  then
+    net=$2
+    echo 'Net UUID is: ' $net
+fi
+if [ ! -z "$3" ] ;  then
+    num=$1
+    echo 'Number of requested instances is: ' $num
+fi
+
+
+outd="./report/"
+mkdir $outd
+output="$outd/$net.out"
+
+echo ''
+echo 'Booting instances'
+echo "nova boot --flavor m1.tiny --image $image --nic net-id=$net vm1 --num-instances $num"
+echo ''
+
+nova boot --flavor m1.tiny --image $image --nic net-id=$net vm1 --num-instances $num
+
+echo "Waiting for IP addresses"
+
+ips=$(nova list | grep ACTIVE  | grep = | cut -d'=' -f2- | cut -d' ' -f1)
+
+nips=$(echo $ips | wc -w)
+
+if [[ $nips -ne $num ]] ; then
+    echo "WARN: Number of instances with IP $nips is not equal to the number of requested instances $num"
+fi
+
+for ip in "${ips[@]}"; do
+  echo "Checking ${ip} reachability"
+  ./ping-reachability.sh $ip $retries "qdhcp-$net" $output
+done
+
