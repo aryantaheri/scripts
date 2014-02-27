ed922316397fc34b527ed7831d047e4778c09160
Aryan <aryantaheri@gmail.com>
2014-02-27 16:24:24 +0100

    Renaming and adding bridge cleaning

--- clean-network.sh
+++ clean-network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 brs=$(ovs-vsctl show | grep "Bridge br"  | awk '{ print $2}')
-brs2=$(ovs-vsctl show | grep "Bridge \"br"  | awk '{ print $2}')
+brs2=$(ovs-vsctl show | grep "Bridge \"br"  | awk '{ print $2}' | cut -d'"' -f2-2)
 qvs=$(ifconfig -a | grep qv | awk '{ print $1}')
 qbs=$(ifconfig -a | grep qb | awk '{ print $1}')
 taps=$(ifconfig -a | grep tap | awk '{ print $1}')
@@ -25,17 +25,17 @@ done
 echo "deleteing qv* interfaces"
 for qv in $qvs ; do
     echo "ip link delete $qv"
-    ip link delete $qv
+#    ip link delete $qv
 done
 
 echo "deleteing qb* interfaces"
 for qb in $qbs ; do
     echo "ip link delete $qb"
-    ip link delete $qb
+#    ip link delete $qb
 done
 
 echo "deleteing tap* interfaces"
 for tap in $taps ; do
     echo "ip link delete $tap"
-    ip link delete $tap
+#    ip link delete $tap
 done
