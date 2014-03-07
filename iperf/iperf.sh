cd05878dd49f8a0b030cab7541f74e53a6d2b44d
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-03-07 19:22:45 +0100

    iperf

--- iperf/iperf.sh
+++ iperf/iperf.sh
@@ -5,7 +5,8 @@ ns=$2
 tcp_out=$3
 udp_out=$4
 
-key="/home/aryan/scripts/iperf/haisen.pem"
+#key="/home/aryan/scripts/iperf/haisen.pem"
+key="/home/aryan/.ssh/id_rsa"
 user="root"
 opt="StrictHostKeyChecking no"
 
@@ -18,8 +19,8 @@ iperf_client_tcp_cmd="iperf -c $ip  -r -y c -t $iperf_time"
 iperf_server_udp_cmd="nohup iperf -s -u > iperf.log 2>&1 &"
 iperf_client_udp_cmd="iperf -c $ip  -r -u -y c -t $iperf_time"
 
-ns_prefix="sudo ip netns exec $ns"
-
+#ns_prefix="sudo ip netns exec $ns"
+ns_prefix=""
 
 # run iperf TCP server
 echo "sudo ip netns exec $ns ssh $user@$ip $opt -i $key \"$iperf_server_tcp_cmd\""
@@ -29,12 +30,14 @@ rc=$?
 if [[ $rc -eq 0 ]] ; then
     sleep 2
     echo "$ns_prefix $iperf_client_tcp_cmd"
-    $ns_prefix $iperf_client_tcp_cmd
+    $ns_prefix $iperf_client_tcp_cmd >> $tcp_out
 
     $ns_prefix ssh $user@$ip -o "$opt" -i $key "$iperf_kill_cmd"
     echo "Done"
 
 else
+    echo "-,-,-,$ip,5001,-,-,-,-" >> $tcp_out
+    echo "-,-,5001,$ip,-,-,-,-,-" >> $tcp_out
     echo "Failure"
 fi
 
@@ -47,12 +50,16 @@ rc=$?
 if [[ $rc -eq 0 ]] ; then
     sleep 2
     echo "$ns_prefix $iperf_client_udp_cmd"
-    $ns_prefix $iperf_client_udp_cmd | awk -F',' '{if($10 !=  "") { print $0 }}' 
+    $ns_prefix $iperf_client_udp_cmd >> $udp_out
+    #| awk -F',' '{if($10 !=  "") { print $0 }}'  >> $udp_out
 
     $ns_prefix ssh $user@$ip -o "$opt" -i $key "$iperf_kill_cmd"
     echo "Done"
 
 else
+    echo "-,-,-,$ip,5001,-,-,-,-" >> $udp_out
+    echo "-,$ip,5001,-,-,-,-,-,-,-,-,-,-,-" >> $udp_out
+    echo "-,-,5001,$ip,-,-,-,-,-,-,-,-,-,-" >> $udp_out
     echo "Failure"
 
 fi
